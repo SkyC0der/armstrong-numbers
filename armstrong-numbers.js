ad78c16458667e51d3cdb5623f5787f75d462bad
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-05-20 15:44:34 +0100

    Created Armstrong number function

--- armstrong-numbers.js
+++ armstrong-numbers.js
@@ -1,4 +1,9 @@
-
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
+const isArmstrongNumber = (num) => {
+ let length = num.toString().length;
+ let digit = Array.from(String(num), Number);
+ let fixed = digit.map((product) => product**length);
+ let value =fixed.reduce((a,b) => a + b);
+ let done = parseInt(value,10)
+ return num === done;
 };
+module.exports = isArmstrongNumber;
